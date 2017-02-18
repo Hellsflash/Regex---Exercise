0c96c44a23ae56dc0a7274d869bad10a64eb7a5c
Mario <Mario@MARIO-PC>
2017-02-18 21:49:03 +0200

    Add project files.

---
+++ 02. Extract sentences by keyword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.Extract_sentences_by_keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+            var text = Console.ReadLine().Split(new[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var pattern = "\\b" + word + "\\b";
+            Regex regex = new Regex(pattern);
+            foreach (var santance in text)
+            {
+                if (regex.IsMatch(santance))
+                {
+                    Console.WriteLine(santance.Trim());
+                }
+            }
+        }
+    }
+}
